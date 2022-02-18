32b0802d580a92ffbd241c6ec8a6803d6a7510e4
kamil-fijalski <kamil.fijalski@outlook.com>
2022-02-18 01:50:21 +0100

    Extract Views

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -14,39 +14,71 @@ struct ContentView: View {
     @State private var game: Game = Game()
     
     var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                InstructionView(game: $game)
+                SliderView(sliderValue: $sliderValue)
+                    .padding()
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+            }
+        }
+    }
+}
+
+struct InstructionView: View {
+    
+    @Binding var game: Game
+    
+    var body: some View {
         VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
-                    .bold()
-                    .font(.body)
-                    .padding()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-                    .font(.body)
-                    .padding()
-            }
-            Button(action: {
-                self.alertIsVisible = true
-            }) {
-                Text("Hit Me")
-            }
-            .alert("Hello Swift!", isPresented: $alertIsVisible) {
-                Button("Awesome!") { }
-            } message: {
-                let roundedValue: Int = Int(sliderValue.rounded())
-                Text("The slider values is: \(roundedValue)\n" + "Your points: \(game.points(sliderValue: roundedValue))")
-            }
+            InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            NumberText(number: game.target)
+        }
+    }
+}
+
+struct SliderView: View {
+    
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            LabelSliderText(numText: "1")
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            LabelSliderText(numText: "100")
+        }
+    }
+}
+
+struct HitMeButton: View {
+    
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        Button(action: {
+            alertIsVisible = true
+        }) {
+            ButtonLook(text: "Hit Me")
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+            })
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert("Hello Swift!", isPresented: $alertIsVisible) {
+            Button("Awesome!") { }
+        } message: {
+            let roundedValue: Int = Int(sliderValue.rounded())
+            Text("The slider values is: \(roundedValue)\n" + "Your points: \(game.points(sliderValue: roundedValue))")
         }
     }
 }
@@ -56,5 +88,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
         ContentView()
             .previewLayout(.fixed(width: 568, height: 320))
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
